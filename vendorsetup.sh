a6978c66591227f26d2b3108d7f2332e181bbba4
scarsed <kimaniwilber@gmail.com>
2025-04-28 15:23:53 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,11 +36,6 @@ export OF_STATUS_INDENT_RIGHT=48
 export OF_ALLOW_DISABLE_NAVBAR=0
 export OF_CLOCK_POS=1
 
-# maintainer 
-export OF_MAINTAINER_AVATAR="$(gettop)/device/tecno/AD8/maintainer_avatar.png"
-cp "${OF_MAINTAINER_AVATAR}" "$(gettop)/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About/maintainer.png"
-export OF_MAINTAINER="Nazephyrus"
-
 # Important build settings
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
